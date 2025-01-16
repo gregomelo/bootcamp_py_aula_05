7ef4974325ef9c903f9efbd6ffe2ea35f56eb1b4
Greg Oliveira <gregoryomelo@gmail.com>
2025-01-16 20:09:21 -0300

    perf(solution_datatable): alterada lógica para processamento dos dados em chunks
    A biblioteca datatable não possui nativamente o recurso de processamento em chunks. Para isso foi necessário alguns ajustes no código e na lógica para calcular a média.

--- src/solution_datatable.py
+++ src/solution_datatable.py
@@ -12,52 +12,6 @@ CONCURRENCY: int = cpu_count()
 CHUNKSIZE: int = int(NUM_ROWS_TO_CREATE * 0.1)
 
 
-def process_chunk_datatable(dataframe_table: dt.Frame) -> dt.Frame:
-    """
-    Processa um chunk de dados utilizando a biblioteca datatable.
-
-    O método realiza operações de agregação como cálculo de mínimo, máximo, soma e
-    contagem para os valores da coluna `measure`, agrupados pela coluna `station`.
-
-    Parameters
-    ----------
-    dataframe_table : dt.Frame
-        Um DataFrame do datatable contendo os dados do chunk a ser processado. Deve
-        incluir pelo menos as colunas `station` e `measure`.
-
-    Returns
-    -------
-    dt.Frame
-        Um DataFrame do datatable contendo os valores agregados por `station`:
-        - `station`: Chave de agrupamento.
-        - `min`: Valor mínimo da métrica.
-        - `max`: Valor máximo da métrica.
-        - `sum`: Soma dos valores da métrica.
-        - `count`: Número de registros para a chave de agrupamento.
-
-    Notes
-    -----
-    - Esta função é otimizada para processamento em chunks, permitindo lidar com
-        grandes volumes de dados sem sobrecarregar a memória.
-    - A agregação é realizada em paralelo, utilizando a estrutura interna da
-        biblioteca datatable para melhor desempenho.
-    """
-    df = dataframe_table
-
-    aggregated_df: dt.Frame = df[
-        :,
-        {
-            "min": dt.min(dt.f.measure),
-            "max": dt.max(dt.f.measure),
-            "sum": dt.sum(dt.f.measure),
-            "count": dt.count(),
-        },
-        dt.by("station"),
-    ]
-
-    return aggregated_df
-
-
 def create_df_with_datatable(
     filename: Path, total_linhas: int, chunksize: int = CHUNKSIZE
 ) -> dt.Frame:
@@ -104,9 +58,10 @@ def create_df_with_datatable(
     - A soma e a contagem são utilizadas para calcular a média global corretamente.
     - O uso de chunks permite processar grandes volumes de dados sem sobrecarregar a memória.
     """
-    parcial_data = []
-    rows_to_skip = 0
+    parcial_data: list = []
+    rows_to_skip: int = 0
     while rows_to_skip < total_linhas:
+        # Lendo o arquivo em chunks
         df: dt.Frame = dt.fread(
             file=filename,
             nthreads=CONCURRENCY,
@@ -114,13 +69,40 @@ def create_df_with_datatable(
             skip_to_line=rows_to_skip,
             max_nrows=chunksize,
         )
+        # Caso tenhamos linhas, processamentos o chunk
         if df.nrows > 0:
-            df_parcial_aggregated: dt.Frame = process_chunk_datatable(df)
+            df_parcial_aggregated: dt.Frame = df[
+                :,
+                {
+                    "min": dt.min(dt.f.measure),
+                    "max": dt.max(dt.f.measure),
+                    "sum": dt.sum(dt.f.measure),
+                    "count": dt.count(),
+                },
+                dt.by("station"),
+            ]
+
             parcial_data.append(df_parcial_aggregated)
 
-        rows_to_skip = rows_to_skip + chunksize
+            # Combinando os resultados parciais para reduzir a memória utilizada
+            df_process: dt.Frame = dt.rbind(parcial_data, force=True)
 
-    df_process = dt.rbind(parcial_data, force=True)
+            # Processando os resultados parciais
+            df_process = df_process[
+                :,
+                {
+                    "min": dt.min(dt.f.min),
+                    "max": dt.max(dt.f.max),
+                    "sum": dt.sum(dt.f.sum),
+                    "count": dt.sum(dt.f.count),
+                },
+                dt.by("station"),
+            ]
+
+            # Preparando os dados para a próxima iteração
+            parcial_data = [df_process]
+
+        rows_to_skip = rows_to_skip + chunksize
 
     final_aggregated_df = dt.Frame = df_process[
         :,
