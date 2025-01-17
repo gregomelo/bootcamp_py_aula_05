c6714beea6929704515fbe42756b1cccfb16e629
Greg Oliveira <gregoryomelo@gmail.com>
2025-01-16 21:49:18 -0300

    feat(run_tests): adicionada a solução com datatable
    n

--- src/run_tests.py
+++ src/run_tests.py
@@ -9,20 +9,25 @@ from create_measurements import (
     estimate_file_size,
 )
 from record_result import record_result
+from solution_datatable import create_df_with_datatable
 from solution_pandas import CHUNKSIZE, create_df_with_pandas
 from solution_polars import create_polars_df_streaming
 
-quantidade_linhas = [100_000 * 10**x for x in range(1, 6)]
+quantidade_linhas = [100_000 * 10**x for x in range(1, 5)]
 
 if __name__ == "__main__":
 
     weather_station_names: List[str] = build_weather_station_name_list()
+
     for quantidade_linha in quantidade_linhas:
         # Gerando arquivos teste
         print(estimate_file_size(weather_station_names, quantidade_linha))
         build_test_data(weather_station_names, quantidade_linha)
+
         print("Arquivo de teste finalizado...\n\n")
+
         print(f"Iniciando testes com {quantidade_linha:,}...\n\n")
+
         record_result(
             "pandas",
             quantidade_linha,
@@ -33,11 +38,20 @@ if __name__ == "__main__":
         )
 
         record_result(
-            "polars",
+            "Polars",
             quantidade_linha,
             create_polars_df_streaming,
             filename=FILENAME_OUTPUT,
             chunksize=CHUNKSIZE,
         )
 
-        print(f"Finaliznado testes com {quantidade_linha:,}...\n\n")
+        record_result(
+            "datatable",
+            quantidade_linha,
+            create_df_with_datatable,
+            filename=FILENAME_OUTPUT,
+            total_linhas=quantidade_linha,
+            chunksize=CHUNKSIZE,
+        )
+
+        print(f"Finalizando testes com {quantidade_linha:,}...\n\n")
